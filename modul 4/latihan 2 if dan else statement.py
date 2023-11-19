2e1f92e6c3da36081ef74e3e72ffb970584ef2df
DiditAdiyansah <147999327+DiditAdiyansah@users.noreply.github.com>
2023-11-19 11:18:51 +0700

    Add files via upload

---
+++ modul 4/latihan 2 if dan else statement.py
@@ -0,0 +1,22 @@
+# If dan Else Statement
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+nama = input("Siapa nama anda? ")
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+# 1. program if inline
+# if nama=="ucup" : print("Kamu Ganteng abieeez!!!!")
+# print(f"Terima kasih {nama}")
+# 2. program if indentation
+# if nama=="ucup":
+# print("kamu ganteng abieeeez!") #
+print("pria tampan se jabodetabek no.2!") #
+print(f"no.1 nya didit ya {nama}")
+# 3. Else Statement
+if nama=="otong":
+    print("hai otooong, si keren!!!")
+else:
+    print("Ah tapi rambut lu ga mullet ga keren! :(")
+    print("besok mullet ya dun")
